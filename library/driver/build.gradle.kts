42ae120e43a62183762dd99e3d34c3d928f25faf
Matthew Nelson <developer@matthewnelson.io>
2023-09-15 09:40:55 -0400

    Fix typos

--- library/driver/build.gradle.kts
+++ library/driver/build.gradle.kts
@@ -174,7 +174,7 @@ private class JdbcRepack {
         }
 
         // Only want the jar file and not xerial/sqlite-jdbc dependency
-        // b/c we're using a fork (Willena/sqlite-jdbc-crypt)
+        // b/c we're using a custom build which uses SQLite3MultipleCiphers
         private val repackSqliteDriver by lazy {
             val jarFile = configJDBCRepack.files.first { file ->
                 file.absolutePath.contains(depSQLDelightDriver.group.toString())
