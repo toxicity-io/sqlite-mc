2462b90b5176c07168b48afa4f9384b93468c9fe
Matthew Nelson <developer@matthewnelson.io>
2023-12-01 13:55:19 -0800

    Lower Android `minSdk` to `21` (#96)

--- build-logic/src/main/kotlin/-KmpConfigurationExtension.kt
+++ build-logic/src/main/kotlin/-KmpConfigurationExtension.kt
@@ -65,7 +65,7 @@ fun KmpConfigurationContainerDsl.androidLibrary(
     namespace: String,
     buildTools: String? = "33.0.2",
     compileSdk: Int = 33,
-    minSdk: Int = 23,
+    minSdk: Int = 21,
     javaVersion: JavaVersion = JavaVersion.VERSION_1_8,
     action: (Action<TargetAndroidContainer.Library>)? = null,
 ) {
